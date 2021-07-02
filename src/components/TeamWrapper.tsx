12181c723d8105512fbb42969ab4e52726f95f6f
Ivo Heidelbach <ivo@heidelba.ch>
2021-07-02 10:39:42 +0200

    scroll down when adding a new team

--- src/components/TeamWrapper.tsx
+++ src/components/TeamWrapper.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useCallback, MouseEvent, ReactElement } from 'react';
+import React, { useContext, useCallback, MouseEvent, ReactElement, useRef, useEffect } from 'react';
 import styles from '../style.module.scss';
 import StateContext from '../context/StateContext';
 import trashIcon from '../assets/trash.png';
@@ -37,9 +37,22 @@ const TeamWrapper = ({ teamNumber, show, children }: Props) => {
 		[teamNumber, removeTeam]
 	);
 
+	const element = useRef<HTMLDivElement>();
+
+	useEffect(() => {
+		if (activeTeam === teamNumber) {
+			element.current?.scrollIntoView({
+				behavior: 'smooth',
+				block: 'start',
+			});
+		}
+		// only scroll when a new team is added
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+
 	if (show) {
 		return (
-			<div className={styles.teamWrapper} onClick={() => setActiveTeam(teamNumber)}>
+			<div className={styles.teamWrapper} onClick={() => setActiveTeam(teamNumber)} ref={element}>
 				<div className={styles.teamWrapperHeader}>
 					<h2>Team {teamNumber + 1}</h2>
 					<button onClick={toggle} className={styles.teamWrapperButton} data-tip="Toggle Team">
